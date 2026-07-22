84210127a819e3693c0f10d94b8248a6ea6adfbe
Guilherme Cunha <guicunhasou@gmail.com>
2026-07-22 04:47:22 -0300

    v.2.14.3: ajuste da logo

--- src/components/Rodape/Rodape.tsx
+++ src/components/Rodape/Rodape.tsx
@@ -13,7 +13,7 @@ function Rodape() {
           rel="noopener noreferrer"
           aria-label="Abrir o repositório do portfólio em uma nova aba"
         >
-          Versão atual 2.14.2
+          Versão atual 2.14.3
         </a>
       </p>
     </footer>
